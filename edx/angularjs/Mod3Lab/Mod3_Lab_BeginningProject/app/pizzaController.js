b6fb0b0412116261f1dafc35f4955d6fd9f467df
adagloria <crossxcell99@gmail.com>
2018-03-02 16:55:04 +0100

    angularjs

---
+++ edx/angularjs/Mod3Lab/Mod3_Lab_BeginningProject/app/pizzaController.js
@@ -0,0 +1,8 @@
+app.controller('pizzaController', [
+    '$scope',
+    function ($scope) {
+        $scope.model = { title: 'Pizza Builder',
+availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 
+'Mushrooms', 'Onion', 'Olives', 'Green Peppers']};
+    }
+]);
